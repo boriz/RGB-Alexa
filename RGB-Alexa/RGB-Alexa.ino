567a5bdf02b605fb8328b277fd33107d4ca78292
Boris <cherkasskiy@gmail.com>
2021-12-26 22:52:09 -0700

    Switched to FastLED library.
    Added chase effect with configurable colors.
    Tested with 300 LED strip.

--- RGB-Alexa/RGB-Alexa.ino
+++ RGB-Alexa/RGB-Alexa.ino
@@ -1,6 +1,6 @@
 #include <WiFi.h>
-#include <Adafruit_NeoPixel.h>
 #include <Preferences.h>
+#include <FastLED.h>
 
 // ESP alexa library
 //#define ESPALEXA_ASYNC
@@ -12,18 +12,24 @@
 #include "config.h"
 
 // RGB strip is connected to pin 13
-#define DO_PIN 13
+#define LED_PIN 13
+#define NUM_LEDS 300 
 
-// Callback functions prototype
+// Callbacks and function prototype
 void Callback_LightColor(EspalexaDevice* dev);
 void Callback_Rainbow(EspalexaDevice* dev);
+void Callback_ChaseColor(EspalexaDevice* dev); // Chase light calback
+void Task_Alexa (void * parameter); // Alexa task
 
 // Other global variables
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(150, DO_PIN, NEO_GRB + NEO_KHZ800);
+CRGB leds[NUM_LEDS];
 Espalexa espalexa;
 int display_mode = 0;
-uint32_t light_color = 0;
-uint32_t light_color_new = 0;
+uint8_t rainbow_brightness = 255;
+CRGB light_color = 0;
+CRGB light_color_new = 0;
+CRGB chase_color = 0;
+TaskHandle_t h_task_alexa;
 
 // Preferneces 
 //uint16_t color;
@@ -36,17 +42,15 @@ void setup()
   Serial.begin(115200);
   Serial.println();
   Serial.println("Setup start");
-
-  // Initialize random generator
-  randomSeed(analogRead(0));  
   
   // Initialize all pixels to 'off'
-  strip.begin();
+  FastLED.addLeds<NEOPIXEL, LED_PIN>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
 
   // The whimpy power supply can't handle a full power, limit brightness a bit
-  strip.setBrightness(127); // 0 = off; 255=max brightness
-  allSet(0);
-  strip.show(); 
+  FastLED.setBrightness(255); // 0 = off; 255 = max brightness
+  FastLED.clear();
+  fill_solid(leds, NUM_LEDS, CRGB::Red);
+  FastLED.show();
  
   // Connect to WiFi access point.
   Serial.print("MAC Address: ");
@@ -59,8 +63,7 @@ void setup()
   WiFi.begin(WLAN_SSID, WLAN_PASS);
   while ( WiFi.status() != WL_CONNECTED ) 
   {
-    int del = random(100, 500);
-    delay(del);
+    delay(500);
     Serial.print(".");
 
     // Timeout?
@@ -74,13 +77,16 @@ void setup()
   Serial.println();
   Serial.print("WiFi connected. IP address: ");
   Serial.println(WiFi.localIP());
+  fill_solid(leds, NUM_LEDS, CRGB::Green); 
+  FastLED.show();
 
   // Add alexa devices
   espalexa.addDevice("RGB light", Callback_LightColor, EspalexaDeviceType::color, 0);
   espalexa.addDevice("RGB rainbow", Callback_Rainbow, EspalexaDeviceType::onoff, 0);
+  espalexa.addDevice("RGB chase", Callback_ChaseColor, EspalexaDeviceType::color, 0);
   espalexa.begin();
 
-  light_color_new = strip.Color(10, 10, 10);
+  light_color_new = CRGB(0, 0, 255);
     
     // Load defaults
   //prefs.begin("app_set", false);
@@ -91,6 +97,11 @@ void setup()
   // Close prefernces
   //prefs.end();
 
+  // Create alexa task
+  // It is not critical, so can run on protocol core (core 0)
+  // Task, Name, stack, parameter, priority, handle, core
+  xTaskCreatePinnedToCore(Task_Alexa, "Alexa", 10000, NULL, 1, &h_task_alexa, 0);
+
   Serial.println("Setup end");
 }
 
@@ -99,30 +110,86 @@ void setup()
 void loop() 
 {
   if (display_mode == 0)
-  {
+  {    
     // light mode
     if (light_color != light_color_new)
     {
-      colorWipe(light_color_new, 30);
+      Serial.println("Loop LightColor. New color");
+      Light_ColorWipe(light_color_new, 10);
       light_color = light_color_new;
     }
-  }  
+    
+    // Update LEDs periodically just in case
+    FastLED.show();
+    delay(100);
+  } 
+  else if (display_mode == 1) 
+  {
+    // Rainbow mode
+    int effect = random(1, 4);
+    
+    switch(effect)
+    {       
+      case 1:
+        // Chase with rainbow colors
+        Serial.println("Loop Rainbow. Rainbow_TheaterChase");
+        Rainbow_TheaterChase(30);
+        break;
+          
+      case 2:
+        // Standard rainbow
+        Serial.println("Loop Rainbow. Rainbow");
+        Rainbow(200);
+        break;
+          
+      case 3:
+        // Equal ranbow
+        Serial.println("Loop Rainbow. Rainbow_Cycle");
+        Rainbow_Cycle(30);
+        break;
+    }    
+  }
+  else if (display_mode == 2) 
+  {
+    // Chase mode
+    Chase_Theater(chase_color, 30);
+  }
+  else
+  {
+    // Wrong mode
+    display_mode = 0;
+  }
 
-  strip.show();
-  espalexa.loop();
-  delay(10);
+  // Short delay
+  delay(1);
 }
 
 
+// ================================================================
+// Tasks
+void Task_Alexa (void * parameter)
+{
+  Serial.printf("Alexa task. Core: %d; Freq: %d MHz \n", xPortGetCoreID(), getCpuFrequencyMhz());
+
+  while (true)
+  {
+    espalexa.loop();
+    delay(1);
+  }
+}
+
+
+// ================================================================
+// Callbacks
 // Alexa Light color callback
 void Callback_LightColor(EspalexaDevice* d) 
 {   
-  Serial.printf("Alexa Light callback. Device:%s; Bright:%d, Red:%d, Green:%d, Blue:%d \n", d->getName(), d->getValue(), d->getR(), d->getG(), d->getB());
+  Serial.printf("LightColor. Callback. Device:%s; Bright:%d, Red:%d, Green:%d, Blue:%d \n", d->getName(), d->getValue(), d->getR(), d->getG(), d->getB());
 
   // Alexa sets odd colors when turing light on by using "on" command
   // Just remeber if we actualy got ON comnmand
   bool on_command = (d->getValue()== 255) && (d->getR() == 254) && (d->getG() == 221) && (d->getB() == 80);
-  Serial.printf("ON command? %d \n", on_command);  
+  Serial.printf("LightColor. Callback. ON command? %d \n", on_command);  
   
   // Limit the brightness
   uint8_t lim_bright = d->getValue();
@@ -132,180 +199,229 @@ void Callback_LightColor(EspalexaDevice* d)
   uint8_t lim_g = d->getG() * lim_bright / 255;
   uint8_t lim_b = d->getB() * lim_bright / 255;
 
-  Serial.printf("Colors=Red:%d, Green:%d, Blue:%d \n", lim_r, lim_g, lim_b);
+  Serial.printf("LightColor. Callback. Colors=Red:%d, Green:%d, Blue:%d \n", lim_r, lim_g, lim_b);
 
   // Switching on
   if (on_command || display_mode != 0)
   {      
-    Serial.println("Received Light command");    
-    display_mode = 0;
+    Serial.println("LightColor. Callback. Received Light ON command");    
+    light_color_new = CRGB(lim_r, lim_g, lim_b);
+    display_mode = 0;    
     return;
   }
 
   // Trying to turn it off?
   if (d->getValue() == 0)
   {      
-    Serial.println("Receive Light off command");
-    light_color_new = strip.Color(0, 0, 0);
+    Serial.println("LightColor. Callback. Receive Light OFF command");
+    light_color_new = CRGB(0, 0, 0);
+    display_mode = 0;
     return;     
   }
 
   // Just changing color.
-  Serial.println("Update color");
-  light_color_new = strip.Color(lim_r, lim_g, lim_b);
+  light_color_new = CRGB(lim_r, lim_g, lim_b);
+  display_mode = 0;
 }
 
 
 void Callback_Rainbow(EspalexaDevice* d) 
 {
-  ;
+  Serial.printf("Rainbow. Callback. Device:%s; Bright:%d, Red:%d, Green:%d, Blue:%d \n", d->getName(), d->getValue(), d->getR(), d->getG(), d->getB());
+  
+  // Trying to turn it off?
+  if (d->getValue() == 0)
+  {      
+    Serial.println("Rainbow. Callback. Receive Rainbow OFF command");
+    light_color_new = CRGB(0, 0, 0);
+    display_mode = 0;
+    return;     
+  }
+
+  // On command or changing brighness, just update values
+  rainbow_brightness = d->getValue();
+  display_mode = 1;
+}
+
+
+void Callback_ChaseColor(EspalexaDevice* d) 
+{   
+  Serial.printf("ChaseColor. Callback. Device:%s; Bright:%d, Red:%d, Green:%d, Blue:%d \n", d->getName(), d->getValue(), d->getR(), d->getG(), d->getB());
+
+  // Alexa sets odd colors when turing light on by using "on" command
+  // Just remeber if we actualy got ON comnmand
+  bool on_command = (d->getValue()== 255) && (d->getR() == 254) && (d->getG() == 221) && (d->getB() == 80);
+  Serial.printf("ChaseColor. Callback. ON command? %d \n", on_command);  
+  
+  // Limit the brightness
+  uint8_t lim_bright = d->getValue();
+
+  // Calcaulte display color based on brightness
+  uint8_t lim_r = d->getR() * lim_bright / 255;
+  uint8_t lim_g = d->getG() * lim_bright / 255;
+  uint8_t lim_b = d->getB() * lim_bright / 255;
+
+  Serial.printf("ChaseColor. Callback. Colors=Red:%d, Green:%d, Blue:%d \n", lim_r, lim_g, lim_b);
+
+  // Switching on
+  if (on_command || display_mode != 2)
+  {      
+    Serial.println("ChaseColor. Callback. Received Chase ON command");    
+    chase_color = CRGB(lim_r, lim_g, lim_b);
+    display_mode = 2;
+    return;
+  }
+
+  // Trying to turn it off?
+  if (d->getValue() == 0)
+  {      
+    Serial.println("ChaseColor. Callback. Receive Chase OFF command");
+    light_color_new = CRGB(0, 0, 0);
+    display_mode = 0;
+    return;     
+  }
+
+  // Just changing color.
+  chase_color = CRGB(lim_r, lim_g, lim_b);
+  display_mode = 2;
 }
 
 
 // ================================================================
 // Fill the dots one after the other with a color
-void colorWipe(uint32_t c, uint8_t wait) 
+void Light_ColorWipe(CRGB c, uint8_t wait) 
 {
-  for(uint16_t i=0; i<strip.numPixels(); i++) 
+  for(uint16_t i = 0; i < NUM_LEDS; i++) 
   {
-      strip.setPixelColor(strip.numPixels() - i - 1, c);
-      strip.show();
+      leds[NUM_LEDS - i - 1] = c;
+      FastLED.show();
       delay(wait);
-  }
-}
-
-
-// Set all pixels to the same color
-void allSet(uint32_t c)
-{
-  for (int i=0; i < strip.numPixels(); i++) 
-  {
-    strip.setPixelColor(i, c);
-  }
-  strip.show();
-}
-
-
-// Running dots
-void runningDots(uint32_t c, uint32_t dots, uint8_t wait) 
-{
-  for (int q=0; q < strip.numPixels(); q++) 
-  {
-    // All off
-    for (int i=0; i < strip.numPixels(); i++) 
-    {
-      strip.setPixelColor(i, 0);
-    }
-    
-    // Dots on
-    for (int i=0; i < dots; i++) 
-    {
-      strip.setPixelColor(strip.numPixels() - (i + q) - 1, c);
-    }
-    strip.show();
-   
-    delay(wait);   
+      if (display_mode != 0) 
+      {
+        Serial.println("Light_ColorWipe cancelled.");
+        return;
+      }
   }
 }
 
 
 // Rainbow effect
-void rainbow(uint8_t wait) {
-  uint16_t i, j;
-
-  for(j=0; j<256; j++) 
+void Rainbow(uint8_t wait) 
+{  
+  for(int j = 0; j < 256; j++) 
   {
-    for(i=0; i<strip.numPixels(); i++) 
+    for(int i = 0; i < NUM_LEDS; i++) 
     {
-      strip.setPixelColor(i, Wheel((i+j) & 255));
+      leds[i] = Wheel((i+j) & 255);
     }
-    strip.show();
+    FastLED.show();
     delay(wait);
+    if (display_mode != 1) 
+    {
+      Serial.println("Rainbow cancelled.");
+      return;
+    }
   }
 }
 
 
 // Slightly different, this makes the rainbow equally distributed throughout
-void rainbowCycle(uint8_t wait) {
-  uint16_t i, j;
-
-  for(j=0; j < 256*5; j++) 
+void Rainbow_Cycle(uint8_t wait) {
+  for(int j=0; j < 256 * 5; j++) 
   { 
     // 5 cycles of all colors on wheel
-    for(i=0; i< strip.numPixels(); i++) 
+    for(int i = 0; i < NUM_LEDS; i++) 
     {
-      strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
+      leds[i] = Wheel(((i * 256 / NUM_LEDS) + j) & 255);
     }
-    strip.show();
+    FastLED.show();
     delay(wait);
-  }
-}
-
-
-//Theatre-style crawling lights.
-void theaterChase(uint32_t c, uint8_t wait) 
-{
-  for (int j=0; j<50; j++) 
-  {  //do 10 cycles of chasing
-    for (int q=0; q < 3; q++) 
+    if (display_mode != 1) 
     {
-      for (int i=0; i < strip.numPixels(); i=i+3) 
-      {
-        strip.setPixelColor(i+q, c);    //turn every third pixel on
-      }
-      strip.show();
-     
-      delay(wait);
-     
-      for (int i=0; i < strip.numPixels(); i=i+3) 
-      {
-        strip.setPixelColor(i+q, 0);        //turn every third pixel off
-      }
+      Serial.println("Rainbow_Cycle cancelled.");
+      return;
     }
   }
 }
 
 
-//Theatre-style crawling lights with rainbow effect
-void theaterChaseRainbow(uint8_t wait) {
+// Theatre-style crawling lights with rainbow effect
+void Rainbow_TheaterChase(uint8_t wait) {
   for (int j=0; j < 256; j++) 
   {     
     // cycle all 256 colors in the wheel
     for (int q=0; q < 3; q++) 
     {
-        for (int i=0; i < strip.numPixels(); i=i+3) 
+        // turn every third pixel on
+        for (int i=0; i < NUM_LEDS; i=i+3) 
         {
-          strip.setPixelColor(i+q, Wheel( (i+j) % 255));    //turn every third pixel on
+          leds[i+q] = Wheel( (i+j) % 255);    
         }
-        strip.show();
-       
+        FastLED.show();       
         delay(wait);
-       
-        for (int i=0; i < strip.numPixels(); i=i+3) 
+        if (display_mode != 1) 
         {
-          strip.setPixelColor(i+q, 0);        //turn every third pixel off
+          Serial.println("Rainbow_TheaterChase cancelled.");
+          return;
+        }
+
+        // turn every third pixel off
+        for (int i=0; i < NUM_LEDS; i=i+3) 
+        {
+          leds[i+q] = CRGB::Black;        
         }
     }
   }
 }
 
 
+// Theatre-style crawling lights.
+void Chase_Theater(CRGB c, uint8_t wait) 
+{
+  for (int j=0; j < 10; j++) 
+  {
+    for (int q=0; q < 3; q++) 
+    {
+      // turn every third pixel on
+      for (int i=0; i < NUM_LEDS; i=i+3) 
+      {
+        leds[i+q] = c;    
+      }
+      FastLED.show();   
+      delay(wait);
+      if (display_mode != 2) 
+      {
+        Serial.println("Chase_Theater cancelled.");
+        return;
+      }
+  
+      // turn every third pixel off
+      for (int i=0; i < NUM_LEDS; i=i+3) 
+      {
+        leds[i+q] = CRGB::Black;        
+      }
+    }
+  }
+}
+
+
+
 // Input a value 0 to 255 to get a color value.
 // The colours are a transition r - g - b - back to r.
-uint32_t Wheel(byte WheelPos) 
+CRGB Wheel(byte WheelPos) 
 {
   if(WheelPos < 85) 
   {
-   return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+   return CRGB(WheelPos * 3, 255 - WheelPos * 3, 0);
   } 
   else if(WheelPos < 170) 
   {
    WheelPos -= 85;
-   return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+   return CRGB(255 - WheelPos * 3, 0, WheelPos * 3);
   } 
   else 
   {
    WheelPos -= 170;
-   return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+   return CRGB(0, WheelPos * 3, 255 - WheelPos * 3);
   }
 }
